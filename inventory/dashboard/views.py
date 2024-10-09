56eb2f8b316823d98d97feb6b2715e4cac08f39c
calebtetteh2000 <calebtetteh65@gmail.com>
2024-10-09 14:04:16 +0000

    "Updated dashboard views, added product form and media settings, modified templates, and updated user models and views."

--- inventory/dashboard/views.py
+++ inventory/dashboard/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
-@login_required(login_url='user-login')
+@login_required
 def index(request):
     return render(request, 'dashboard/index.html')
 
-@login_required(login_url='user-login')
+@login_required
 def staff(request):
     return render(request, 'dashboard/staff.html')
 
-@login_required(login_url='user-login')
+@login_required
 def products(request):
-    return render(request, 'dashboard/products.html')
+    items = Product.objects.all() # Using ORM
+    # items = Product.objects.raw('SELECT * FROM dashboard_product')
 
-@login_required(login_url='user-login')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-products')
+    else:
+        form = ProductForm()
+    context={
+        'items':items,
+        'form':form,
+    }
+    return render(request, 'dashboard/products.html', context)
+
+@login_required
 def order(request):
     return render(request, 'dashboard/order.html')
 
--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -132,4 +132,10 @@ STATIC_ROOT = (BASE_DIR/"asert/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_ROOT = (BASE_DIR / 'media')
+
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = 'dashboard-index'
+
+LOGIN_URL = 'user-login'
