bb657fe5c72b593e7119740e634a271404949fed
calebtetteh2000 <calebtetteh65@gmail.com>
2024-10-09 19:29:50 +0000

    "Updated inventory dashboard with new forms, views, and templates for products, orders, and staff management; modified user login and registration views; and made changes to various HTML templates."

--- inventory/user/models.py
+++ inventory/user/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     staff = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     address = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=20, null=True)
-    image = models.ImageField(default='girl.jpg', upload_to='Profile_Images')
+    image = models.ImageField(default='default.png', upload_to='Profile_Images')
 
     def __str__(self):
         return f'{self.staff.username}-Profile'
